d22208c4b13a65c786efc6025d52375c03e8f9f4
Jan Qvarnström <jan.qvarnstrom@su.se>
2019-04-29 12:32:42 +0200

    VAULT-54: refactored aspect code not to list getMetaData

--- src/main/groovy/se/su/it/vaulttool/aspect/ExecutionTime.groovy
+++ src/main/groovy/se/su/it/vaulttool/aspect/ExecutionTime.groovy
@@ -12,15 +12,15 @@ import org.aspectj.lang.annotation.Pointcut
 class ExecutionTime {
     Date before
     Date after
-    @Before("within(se.su.it.vaulttool.VaultRestService)")
+    @Before("execution(* se.su.it.vaulttool.VaultRestService.*(..)) && !execution(* se.su.it.vaulttool.VaultRestService.*MetaClass(..))")
     void timeBefore(JoinPoint joinPoint){
         before = new Date()
     }
 
-    @After("within(se.su.it.vaulttool.VaultRestService)")
+    @After("execution(* se.su.it.vaulttool.VaultRestService.*(..)) && !execution(* se.su.it.vaulttool.VaultRestService.*MetaClass(..))")
     void timeAfter(JoinPoint joinPoint){
         after = new Date()
         TimeDuration duration = TimeCategory.minus(after, before)
-        println("${joinPoint.signature.name} took ${duration} to run")
+        println("Measuring execution time for ${joinPoint.signature.name}, took ${duration} to run.")
     }
 }
